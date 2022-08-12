b62ddef7214d6d092b2e00f18ddf79ce3ea45f39
HyeonHan Bae <baehyunhan3@gmail.com>
2022-08-12 23:17:08 +0900

    add

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
+from sqlite3 import Row
 import sys
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
-from PySide2.QtUiTools import loadUiType
 import os
 from pathlib import Path
 import clipboard
 
 CURRENT_DIRECTORY = Path(__file__).resolve().parent
 
-text = 'ABCabc123가나다'
-fontSize = 11
+text = 'ABab12가나'
+fontSize = 30
 choise = 0
 FontListLine = []
-
+fileList = os.listdir('fonts')
 familiesList = []
-
+RowNum = 6
 def load_fonts_from_dir(directory):
     global familiesList
+    print()
+    
     families = set()
+    fileList_TTF = QDir(directory).entryInfoList(["*.ttf"])
+    fileList_TTC = QDir(directory).entryInfoList(["*.ttc"])
+    fileList_OTF = QDir(directory).entryInfoList(["*.otf"])
     familiesList = []
-    for fi in QDir(directory).entryInfoList(["*.ttf"]):
-        _id = QFontDatabase.addApplicationFont(fi.absoluteFilePath())
+    for file in fileList_TTF:
+        _id = QFontDatabase.addApplicationFont(file.absoluteFilePath())
         data = set(QFontDatabase.applicationFontFamilies(_id))
         families |= data
+        data = str(data).replace(' ',' ')
+        familiesList.append(str(data).replace("{'",'').replace("'}",''))
+    print()
+    for file in fileList_TTC:
+        _id = QFontDatabase.addApplicationFont(file.absoluteFilePath())
+        data = set(QFontDatabase.applicationFontFamilies(_id))
+        families |= data
+        data = str(data).replace("{'",'').replace("'}",'').split("', '")
+        for i in range(len(data)):
+            familiesList.append(str(data[i]))
+    print()
+    for file in fileList_OTF:
+        _id = QFontDatabase.addApplicationFont(file.absoluteFilePath())
+        data = set(QFontDatabase.applicationFontFamilies(_id))
+        families |= data
+        data = str(data)
         familiesList.append(str(data).replace("{'",'').replace("'}",''))
     return familiesList
 
@@ -34,7 +55,7 @@ class Window(QWidget):
         global FontListLine
         global choise
         super().__init__()
-        self.title = "폰트 검색기 made by.Hyu-nani V1.5"
+        self.title = "폰트 검색기 made by.Hyu-nani V1.6"
         self.top = 200
         self.left = 500
         self.width = 1000
@@ -63,20 +84,20 @@ class Window(QWidget):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Gulim\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">ABCabc123가나다</span></p></body></html>"))
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:11pt;\">ABab12가나</span></p></body></html>"))
         self.textEdit.setMaximumWidth(1000)
         self.textEdit.setMaximumHeight(100)
 
         self.sizeupBtn = QPushButton(self.centralwidget)
         self.sizeupBtn.setObjectName("sizeupBtn")
         self.sizeupBtn.setText("+")
-        self.sizeupBtn.setFont(db.font("Normal", "Medium Italic", 20))
+        self.sizeupBtn.setFont(db.font("Normal", "Regular", 20))
         self.sizeupBtn.setMaximumWidth(100)
 
         self.sizedownBtn = QPushButton(self.centralwidget)
         self.sizedownBtn.setObjectName("sizedownBtn")
         self.sizedownBtn.setText("-")
-        self.sizedownBtn.setFont(db.font("Normal", "Medium Italic", 20))
+        self.sizedownBtn.setFont(db.font("Normal", "Regular", 20))
         self.sizedownBtn.setMaximumWidth(100)
 
         self.fontSizeLabel = QLabel(self.centralwidget)
@@ -100,11 +121,13 @@ class Window(QWidget):
         layout_lower = QGridLayout()
         groupBox = QGroupBox("This Is Font Box")
 
-        RowNum = 4
+        
         for i in  range(len(families)):
             FontListLine.append(QPushButton(text))
-            FontListLine[i].setFont(db.font(families[i], "Medium Italic", fontSize))
-            FontListLine[i].setMaximumHeight(100)
+            #FontListLine[i].setFont(db.font(familiesList[i], "Regular", fontSize))
+            FontListLine[i].setFont(QFont(familiesList[i], fontSize))
+            FontListLine[i].setMaximumHeight(150)
+            FontListLine[i].setMaximumWidth(600)
             layout_lower.addWidget(FontListLine[i], i // RowNum, i % RowNum)
     
         groupBox.setLayout(layout_lower)
@@ -133,15 +156,19 @@ class Window(QWidget):
 
 
     def indicate(self):
+        global RowNum
          # 글자 가져오기
         text = self.textEdit.toPlainText()
+        if len(text) < 1:
+            QMessageBox.question(self, '알림', '한글자 이상 입력해주세요.', QMessageBox.Yes)
+            return False
         # 폰트 폴어 안에 내용 이름 가져오기
         font_dir = CURRENT_DIRECTORY / "fonts"
         families = load_fonts_from_dir(os.fspath(font_dir))
         db = QFontDatabase()
         for i in  range(len(families)):
             FontListLine[i].setText(text)
-            FontListLine[i].setFont(db.font(families[i], "Medium Italic", fontSize))
+            FontListLine[i].setFont(QFont(familiesList[i], fontSize))
         
     def fontSizeUp(self):
         global fontSize
