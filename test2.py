dd1b801776415730d6ba1f1b80a69d02fc6c7e17
HyeonHan Bae <baehyunhan3@gmail.com>
2022-08-12 16:08:23 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -7,11 +7,16 @@ import time
 from PySide2.QtUiTools import loadUiType
 import os
 from pathlib import Path
+import clipboard
 
 CURRENT_DIRECTORY = Path(__file__).resolve().parent
 
-page = 1
+text = 'ABCabc123가나다'
 fontSize = 11
+choise = 0
+FontListLine = []
+
+families = 0
 
 def load_fonts_from_dir(directory):
     families = set()
@@ -23,9 +28,13 @@ def load_fonts_from_dir(directory):
         familiesList.append(str(data).replace("{'",'').replace("'}",''))
     return familiesList
 
+
 class Window(QWidget):
+    
 
     def __init__(self):
+        global FontListLine
+        global choise
         super().__init__()
         self.title = "폰트 검색기 made by.Hyu-nani V1.5"
         self.top = 200
@@ -35,31 +44,46 @@ class Window(QWidget):
         self.setWindowIcon(QIcon("icon.png"))
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # 폰트 폴어 안에 내용 이름 가져오기
+        font_dir = CURRENT_DIRECTORY / "fonts"
+        families = load_fonts_from_dir(os.fspath(font_dir))
         db = QFontDatabase()
+
         self.centralwidget = QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
+
         self.indicateBtn = QPushButton(self.centralwidget)
         self.indicateBtn.setText("변환")
-        self.indicateBtn.setGeometry(QRect(10, 10, 30, 30))
+        self.indicateBtn.setMaximumWidth(100)
+        self.indicateBtn.setMaximumHeight(100)
         self.indicateBtn.setObjectName("indicateBtn")
+
         self.textEdit = QTextEdit(self.centralwidget)
-        self.textEdit.setGeometry(QRect(100, 10, 561, 51))
         self.textEdit.setObjectName("textEdit")
+        self.textEdit.setMaximumWidth(1000)
+        self.textEdit.setMaximumHeight(100)
+
         self.sizeupBtn = QPushButton(self.centralwidget)
-        self.sizeupBtn.setGeometry(QRect(670, 10, 41, 25))
         self.sizeupBtn.setObjectName("sizeupBtn")
         self.sizeupBtn.setText("+")
-        self.sizeupBtn.setFont(db.font("Helvetica", "Medium Italic", 20))
+        self.sizeupBtn.setFont(db.font("Normal", "Medium Italic", 20))
+        self.sizeupBtn.setMaximumWidth(100)
+
         self.sizedownBtn = QPushButton(self.centralwidget)
-        self.sizedownBtn.setGeometry(QRect(670, 40, 41, 25))
         self.sizedownBtn.setObjectName("sizedownBtn")
         self.sizedownBtn.setText("-")
-        self.sizedownBtn.setFont(db.font("Helvetica", "Medium Italic", 20))
+        self.sizedownBtn.setFont(db.font("Normal", "Medium Italic", 20))
+        self.sizedownBtn.setMaximumWidth(100)
+
         self.fontSizeLabel = QLabel(self.centralwidget)
-        self.fontSizeLabel.setGeometry(QRect(720, 10, 61, 51))
         self.fontSizeLabel.setFrameShape(QFrame.WinPanel)
         self.fontSizeLabel.setAlignment(Qt.AlignCenter)
+        self.fontSizeLabel.setText(str(fontSize))
         self.fontSizeLabel.setObjectName("fontSizeLabel")
+        self.fontSizeLabel.setMaximumHeight(100)
+        self.fontSizeLabel.setFixedWidth(100)
+        self.fontSizeLabel.setMinimumHeight
 
         layout_size = QVBoxLayout()
         layout_size.addWidget(self.sizeupBtn)
@@ -69,34 +93,22 @@ class Window(QWidget):
         layout_upper.addWidget(self.textEdit)
         layout_upper.addLayout(layout_size)
         layout_upper.addWidget(self.fontSizeLabel)
-        
 
         layout_lower = QGridLayout()
         groupBox = QGroupBox("This Is Font Box")
 
-        labelLis = []
-        FontListLine1 = []
-        FontListLine2 = []
-        FontListLine3 = []
-
-        # 폰트 폴어 안에 내용 이름 가져오기
-        font_dir = CURRENT_DIRECTORY / "fonts"
-        families = load_fonts_from_dir(os.fspath(font_dir))
-        db = QFontDatabase()
+        choise = 0
         #styles = db.styles(families[2])
-
-        for i in  range(len(families)-300):
-            labelLis.append(QLabel("Label"))
-            FontListLine1.append(QPushButton("Click Me"))
-            FontListLine1[i].setFont(db.font(families[1+i*3], "Medium Italic", fontSize))
-            FontListLine2.append(QPushButton("Click Me"))
-            FontListLine2[i].setFont(db.font(families[2+i*3], "Medium Italic", fontSize))
-            FontListLine3.append(QPushButton("Click Me"))
-            FontListLine3[i].setFont(db.font(families[3+i*3], "Medium Italic", fontSize))
-            #data = formLayout.addRow(comboList2[i],comboList3[i])
-            layout_lower.addWidget(FontListLine1[i], i, 0)
-            layout_lower.addWidget(FontListLine2[i], i, 1)
-            layout_lower.addWidget(FontListLine3[i], i, 2)
+        RowNum = 4
+        num = []
+        for i in  range(len(families)):
+            FontListLine.append(QPushButton(text))
+            num.append(str(i))
+            FontListLine[i].setFont(db.font(families[i], "Medium Italic", fontSize))
+            FontListLine[i].setMaximumHeight(100)
+            layout_lower.addWidget(FontListLine[i], i // RowNum, i % RowNum)
+            FontListLine[i].clicked.connect(lambda: self.copy(num[i]))
+        
 
         groupBox.setLayout(layout_lower)
         scroll = QScrollArea()
@@ -110,18 +122,23 @@ class Window(QWidget):
         #버튼 작동
         self.sizeupBtn.clicked.connect(self.fontSizeUp)
         self.sizedownBtn.clicked.connect(self.fontSizeDown)
+        self.indicateBtn.clicked.connect(self.indicate)
 
-    def pageNext(self):
-        global page
-        page = page + 1
-        self.pageLabel.setText(str(page))
-
-    def pagePrev(self):
-        global page
-        if page > 1:
-            page = page - 1
-        self.pageLabel.setText(str(page))
-
+    def copy(self, choise):
+        print("!!!")
+        print(str(choise))
+        clipboard.copy(FontListLine[choise])
+    def indicate(self):
+         # 글자 가져오기
+        text = self.textEdit.toPlainText()
+        # 폰트 폴어 안에 내용 이름 가져오기
+        font_dir = CURRENT_DIRECTORY / "fonts"
+        families = load_fonts_from_dir(os.fspath(font_dir))
+        db = QFontDatabase()
+        for i in  range(len(families)):
+            FontListLine[i].setText(text)
+            FontListLine[i].setFont(db.font(families[i], "Medium Italic", fontSize))
+        
     def fontSizeUp(self):
         global fontSize
         fontSize = fontSize + 1
@@ -133,6 +150,8 @@ class Window(QWidget):
         self.fontSizeLabel.setText(str(fontSize))
 
 if __name__ == "__main__":
+    
+
     App = QApplication(sys.argv)
     MainWindow = QMainWindow()
     window = Window()
